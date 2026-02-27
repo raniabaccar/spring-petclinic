7d102e92ac25f665f731b14cfe282d3962f580cd
raniabaccar <rania.baccar55@gmail.com>
2026-02-27 02:32:06 +0100

    Agent 2 Auto-Fix: ABS-7 - Issue with initCreationForm method.

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -82,7 +82,7 @@ class OwnerController {
 	 */
 	@GetMapping("/owners/new")
 	public String initCreationForm() {
-		return "owners/brokenPage"; // <--- We replaced the constant with a bad page name!
+		return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
 	}
 
 	@PostMapping("/owners/new")
